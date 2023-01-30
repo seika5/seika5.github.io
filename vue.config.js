7d185bd85c942f5db2d1f6f817fad988a3ae2093
Ryan Mar <Phyre205@gmail.com>
2023-01-30 09:14:27 -0800

    init test

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = defineConfig({
   transpileDependencies: true,
-
+  publicPath: '/seika5.github.io/',
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
 		}
+  },
+  configureWebpack: {
+    plugins: [new MiniCssExtractPlugin()] 
   }
 })
